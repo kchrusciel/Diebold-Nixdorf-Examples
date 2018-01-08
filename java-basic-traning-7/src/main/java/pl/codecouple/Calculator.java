91defe0404d083ee42d73720ea396b9736800ebf
Chrusciel, Krzysztof <krzysztof.chrusciel@dieboldnixdorf.com>
2018-01-08 11:04:46 +0100

    Add next example

---
+++ java-basic-traning-7/src/main/java/pl/codecouple/Calculator.java
@@ -0,0 +1,4 @@
+package pl.codecouple;
+
+class Calculator {
+}
---
+++ java-basic-traning-7/src/test/java/pl/codecouple/CalculatorSpec.groovy
@@ -0,0 +1,25 @@
+package pl.codecouple
+
+import spock.lang.Specification
+import java.lang.Void as Should
+
+class CalculatorSpec extends Specification {
+
+    Should "add two numbers" () {
+
+    }
+
+    Should "subtract two numbers" () {
+
+    }
+
+    Should "thrown DividingByZero exception when divide by zero" () {
+
+    }
+
+    Should "multiply two numbers" () {
+
+    }
+
+
+}
