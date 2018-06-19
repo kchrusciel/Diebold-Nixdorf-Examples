06433493108b2384c56bb81eea6b4ae12a72e975
Chrusciel, Krzysztof <krzysztof.chrusciel@dieboldnixdorf.com>
2018-06-19 11:43:52 +0200

    Add part 11

---
+++ java-basic-traning-11/src/main/java/pl/codecouple/javapart10/JavaPart10Application.java
@@ -0,0 +1,12 @@
+package pl.codecouple.javapart10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPart10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPart10Application.class, args);
+	}
+}
